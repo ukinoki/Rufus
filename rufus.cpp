390258865559accdac61e9febf23ff54ba48e3c1
ukinoki <serge.laine2@sfr.fr>
2019-10-09 14:34:21 +0200

    09/10/2019

--- rufus.cpp
+++ rufus.cpp
@@ -23,7 +23,8 @@ Rufus::Rufus(QWidget *parent) : QMainWindow(parent)
     Datas::I();
 
     //! la version du programme correspond à la date de publication, suivie de "/" puis d'un sous-n° - p.e. "23-6-2017/3"
-    qApp->setApplicationVersion("09-10-2019/1");       //! la date doit impérativement être composé de date version au format "00-00-0000" / n°version;
+    //! la date doit impérativement être composé de date version au format "00-00-0000" / n°version
+    qApp->setApplicationVersion("09-10-2019/1");
 
     ui = new Ui::Rufus;
     ui->setupUi(this);
