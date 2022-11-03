9a8f91898740c0295129cc59b4926d404b21f02b
Serge Lainé <33565773+ukinoki@users.noreply.github.com>
2022-11-03 04:32:57 +0100

    03/11/2022

--- procedures.cpp
+++ procedures.cpp
@@ -701,9 +701,10 @@ void Procedures::DefinitScriptBackup(QString pathdirdestination, bool AvecImages
  * \brief Procedures::DefinitScriptRestore
  * \param ListNomFiles
  */
-QList<QStringList> Procedures::DefinitListCommandesSQL(QStringList ListNomFiles)
+int Procedures::ExecuteScriptSQL(QStringList ListScripts)
 {
-    QList<QStringList> args;
+    QStringList listpaths;
+    int a = 99;
     QString cheminmysql;
 #ifdef Q_OS_MACX
     cheminmysql = "/usr/local/mysql/bin";           // Depuis HighSierra on ne peut plus utiliser + Dir.absolutePath() + DIR_LIBS2 - le script ne veut pas utiliser le client mysql du package (???)
@@ -730,16 +731,26 @@ QList<QStringList> Procedures::DefinitListCommandesSQL(QStringList ListNomFiles)
             m_settings->setValue(Utils::getBaseFromMode(Utils::Distant) + Dossier_ClesSSL,dirkey);
         keys += " --ssl-ca=" + dirkey + "/ca-cert.pem --ssl-cert=" + dirkey + "/client-cert.pem --ssl-key=" + dirkey + "/client-key.pem";
     }
-    for (int i=0; i<ListNomFiles.size(); i++)
-        if (QFile(ListNomFiles.at(i)).exists())
+    QString command = cheminmysql + "/mysql -u " + login + " -p" MDP_SQL " -h " + host + " -P " + QString::number(db->port()) + keys;
+    for (int i=0; i<ListScripts.size(); i++)
+        if (QFile(ListScripts.at(i)).exists())
         {
-            QStringList arg;
-            QString command = cheminmysql + "/mysql -u " + login + " -p" MDP_SQL " -h " + host + " -P " + QString::number(db->port()) + keys;
-            QString path = ListNomFiles.at(i);
-            arg << command << path;
-            args << arg;
+            QString path = ListScripts.at(i);
+            listpaths << path;
         }
-    return args;
+    QProcess dumpProcess(parent());
+      for (int i=0; i< listpaths.size(); i++)
+      {
+          QString path = listpaths.at(i);
+          dumpProcess.setStandardInputFile(path);
+          dumpProcess.start(command);
+          dumpProcess.waitForFinished(1000000000); /*! sur des systèmes lents, la création de la base prend parfois plus que les 30 secondes que sont la valeur par défaut de l'instruction waitForFinished() */
+          if (dumpProcess.exitStatus() == QProcess::NormalExit)
+              a = dumpProcess.exitCode();
+          if (a != 0)
+              i = listpaths.size();
+      }
+      return a;
 }
 
 /*!
@@ -2353,26 +2364,8 @@ bool Procedures::RestaureBase(bool BaseVierge, bool PremierDemarrage, bool Verif
             db->StandardSQL("CREATE USER IF NOT EXISTS '" LOGIN_SQL "SSL'@'%' IDENTIFIED BY '" MDP_SQL "' REQUIRE SSL");
             db->StandardSQL("GRANT ALL ON *.* TO '" LOGIN_SQL "SSL'@'%' IDENTIFIED BY '" MDP_SQL "' WITH GRANT OPTION");
 
-            int a = 99;
-
             //! Restauration à partir du dossier sélectionné
-            QList<QStringList> args = DefinitListCommandesSQL(listnomsfilestorestore);
-            QProcess dumpProcess(parent());
-            for (int i=0; i< args.size(); i++)
-            {
-                QString path = args.at(i).last();
-                QString command = args.at(i).first();;
-                dumpProcess.setStandardInputFile(path);
-                dumpProcess.start(command);
-                dumpProcess.waitForFinished(1000000000); /*! sur des systèmes lents, la création de la base prend parfois plus que les 30 secondes que sont la valeur par défaut de l'instruction waitForFinished()
-                                                       * et dans ce cas le processus est interrompu avant que toute la base soit créée */
-
-                //qDebug() << Utils::EnumDescription(QMetaEnum::fromType<QProcess::ExitStatus>(), dumpProcess.exitCode()) << "dumpProcess.exitCode()" << dumpProcess.exitCode() << dumpProcess.errorString();
-                if (dumpProcess.exitStatus() == QProcess::NormalExit)
-                    a = dumpProcess.exitCode();
-                if (a != 0)
-                    i = args.size();
-            }
+            int a = ExecuteScriptSQL(listnomsfilestorestore);
             if (a != 0)
             {
                 UpSystemTrayIcon::I()->showMessage(tr("Messages"), tr("Incident pendant la restauration"), Icons::icSunglasses(), 3000);
@@ -2560,24 +2553,9 @@ bool Procedures::RestaureBase(bool BaseVierge, bool PremierDemarrage, bool Verif
                             QString Msg = tr("Suppression de l'ancienne base Rufus en cours");
                             UpSystemTrayIcon::I()->showMessage(tr("Messages"), Msg, Icons::icSunglasses(), 3000);
                             db->VideDatabases();
-                            int a = 99;
 
                             //! Restauration à partir du dossier sélectionné
-                            QList<QStringList> args = DefinitListCommandesSQL(listnomsfilestorestore);
-                            QProcess dumpProcess(parent());
-                            for (int i=0; i< args.size(); i++)
-                            {
-                                QString path = args.at(i).last();
-                                QString command = args.at(i).first();;
-                                dumpProcess.setStandardInputFile(path);
-                                dumpProcess.start(command);
-                                dumpProcess.waitForFinished(1000000000); /*! sur des systèmes lents, la création de la base prend parfois plus que les 30 secondes que sont la valeur par défaut de l'instruction waitForFinished()
-                                                                       * et dans ce cas le processus est interrompu avant que toute la base soit créée */
-                                if (dumpProcess.exitStatus() == QProcess::NormalExit)
-                                    a = dumpProcess.exitCode();
-                                if (a != 0)
-                                    i = args.size();
-                            }
+                            int a = ExecuteScriptSQL(listnomsfilestorestore);
                             if (a != 0)
                                 UpSystemTrayIcon::I()->showMessage(tr("Messages"), tr("Incident pendant la restauration"), Icons::icSunglasses(), 3000);
                             db->setdirimagerie(NomDirStockageImagerie);
@@ -2780,21 +2758,9 @@ bool Procedures::VerifBaseEtRessources()
                 QFile::remove(NomDumpFile);
                 DumpFile.copy(NomDumpFile);
                 emit ConnectTimers(false);
-                QList<QStringList> args = DefinitListCommandesSQL(QStringList() << NomDumpFile);
-                QProcess dumpProcess(parent());
-                for (int i=0; i< args.size(); i++)
-                {
-                    QString path = args.at(i).last();
-                    QString command = args.at(i).first();;
-                    dumpProcess.setStandardInputFile(path);
-                    dumpProcess.start(command);
-                    dumpProcess.waitForFinished(1000000000); /*! sur des systèmes lents, la création de la base prend parfois plus que les 30 secondes que sont la valeur par défaut de l'instruction waitForFinished()
-                                                           * et dans ce cas le processus est interrompu avant que toute la base soit créée */
-                    if (dumpProcess.exitStatus() == QProcess::NormalExit)
-                        a = dumpProcess.exitCode();
-                    if (a != 0)
-                        i = args.size();
-                }                if (a == 0)
+                a = ExecuteScriptSQL(QStringList() << NomDumpFile);
+                QFile::remove(NomDumpFile);
+                if (a == 0)
                 {
                     UpMessageBox::Watch(Q_NULLPTR,tr("Mise à jour effectuée de la base vers la version ") + QString::number(Version));
                     db->initParametresSysteme();
@@ -2823,12 +2789,6 @@ bool Procedures::VerifBaseEtRessources()
                     }
                 req = "update " TBL_MANUFACTURERS " set CorNom = null, CorPrenom = null, CorStatut = null, CorMail = null, CorTelephone = null";
                 DataBase::I()->StandardSQL(req);
-//                DataBase::I()->StandardSQL("ALTER TABLE `rufus`.`Manufacturers` "
-//                "DROP COLUMN `CorTelephone`,"
-//                "DROP COLUMN `CorMail`,"
-//                "DROP COLUMN `CorStatut`,"
-//                "DROP COLUMN `CorPrenom`,"
-//                "DROP COLUMN `CorNom`;");
             }
         }
     }
