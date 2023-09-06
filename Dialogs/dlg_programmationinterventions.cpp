8c118e8226a268faf63cf99f879a757e7fecddfb
Serge Lainé <33565773+ukinoki@users.noreply.github.com>
2023-09-06 04:44:24 +0200

    06/09/2023

--- Dialogs/dlg_programmationinterventions.cpp
+++ Dialogs/dlg_programmationinterventions.cpp
@@ -1508,7 +1508,7 @@ void dlg_programmationinterventions::FicheIntervention(Intervention *interv)
         {
             listbinds[CP_IDIOL_LIGNPRGOPERATOIRE] = m_currentIOL->id();
             listbinds[CP_PWRIOL_LIGNPRGOPERATOIRE] = wdg_pwrIOLspinbox->value();
-            listbinds[CP_CYLIOL_LIGNPRGOPERATOIRE] = wdg_cylIOLspinbox->value();
+            listbinds[CP_CYLIOL_LIGNPRGOPERATOIRE] = (wdg_cylIOLspinbox->isVisible()? wdg_cylIOLspinbox->value() : QVariant());
         }
         else
         {
@@ -1868,6 +1868,9 @@ void dlg_programmationinterventions::CalcRangeBox(IOL *iol)
                     wdg_cylIOLspinbox->setValue(midvalue);
                 }
             }
+        }
+        else {
+            wdg_cylIOLspinbox->setValue(0.00);
         }
     }
     if (!pwrdefined)
