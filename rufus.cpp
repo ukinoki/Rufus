02828fa8397599451113c44214e6c1e05b2e8bf7
Serge Lainé <33565773+ukinoki@users.noreply.github.com>
2024-01-14 14:25:57 +0100

    O8/O1/2024 script rufus-install.sh

--- rufus.cpp
+++ rufus.cpp
@@ -23,7 +23,7 @@ Rufus::Rufus(QWidget *parent) : QMainWindow(parent)
 {
     //! la version du programme correspond à la date de publication, suivie de "/" puis d'un sous-n° - p.e. "23-6-2017/3"
     //! la date doit impérativement être composée au format "00-00-0000" / n°version
-    qApp->setApplicationVersion("07-01-2024/1");
+    qApp->setApplicationVersion("08-01-2024/1");
     ui = new Ui::Rufus;
     ui->setupUi(this);
     setWindowFlags(Qt::Window | Qt::WindowTitleHint | Qt::WindowMinimizeButtonHint | Qt::WindowCloseButtonHint);
@@ -5733,7 +5733,7 @@ void Rufus::VerifLastVersion()
             if (m_MAJBase)
                 text += "\n" + tr("Cette nouvelle version impose une mise à jour de la base de données");
             else
-                text += "\n" + tr("Cette nouvelle version n'impose pas de mise à jour de la base de données et est compatible avec la précédente version de Rufus");
+                text += "\n" + tr("Cette nouvelle version n'impose pas de mise à jour de la base de données et est compatible avec votre version actuelle de Rufus");
             if (!m_compatibiltywithprec)
                 text += "\n" + tr("Après cette mise à jour, tous les postes utilisant Rufus sur cette base devront aussi évoluer vers la nouvelle versionr");
             else
@@ -5758,6 +5758,8 @@ void Rufus::VerifLastVersion()
             m_os = QSysInfo::productType();
             if (m_os == "osx")
                 m_os  = "macos";
+            else if (m_os == "" )
+                m_os = "linux";
             reply->deleteLater();
             data = reply->readAll();
             QDomDocument docxml;
