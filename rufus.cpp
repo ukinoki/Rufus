e04eb86b07c1dd3b216bce5cbf18cd2559b0e6fd
Serge Lainé <33565773+ukinoki@users.noreply.github.com>
2026-07-11 23:26:31 +0200

    Update rufus.cpp

--- rufus.cpp
+++ rufus.cpp
@@ -41,7 +41,7 @@ Rufus::Rufus(QWidget *parent) : QMainWindow(parent)
 {
     //! la version du programme correspond à la date de publication, suivie de "/" puis d'un sous-n° - p.e. "23-6-2017/3"
     //! la date doit impérativement être composée au format "00-00-0000" / n°version
-    qApp->setApplicationVersion("09-07-2026/1");
+    qApp->setApplicationVersion("10-07-2026/1");
     ui = new Ui::Rufus;
     ui->setupUi(this);
     setWindowFlags(Qt::Window | Qt::WindowTitleHint | Qt::WindowMinimizeButtonHint | Qt::WindowCloseButtonHint);
