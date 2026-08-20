b56f408fd92ab9f9645c2f780e088c29d01b7956
Serge Lainé <33565773+ukinoki@users.noreply.github.com>
2026-08-20 16:28:22 +0200

    Update rufus.sql

--- assets/Fichiers/rufus.sql
+++ assets/Fichiers/rufus.sql
@@ -436,7 +436,7 @@ CREATE TABLE `ParametresSysteme` (
 
 LOCK TABLES `ParametresSysteme` WRITE;
 /*!40000 ALTER TABLE `ParametresSysteme` DISABLE KEYS */;
-INSERT INTO `ParametresSysteme` VALUES ('bob',1,1,1,83,
+INSERT INTO `ParametresSysteme` VALUES ('bob',1,1,1,84,
                                         1,77,'2026-07-01',2.60,2.72,
                                         1,NULL,NULL,NULL,NULL,
                                         NULL,NULL,NULL,NULL,NULL,
